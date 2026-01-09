e882b78dd492c0f9725249b2e1f9cbdacf1a44ad
bezzinag <bezzinag@yahoo.com>
2026-01-09 22:09:25 +0100

    for loops

---
+++ 19-for_loops/for.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main()
+{
+    for(int x = 0; x < 10; x++)
+    {
+        printf("%d\n", x);
+        Sleep(1000);
+        
+    }
+
+    printf("hello world\n");
+    return 0;
+}
