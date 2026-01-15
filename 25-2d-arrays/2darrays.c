399f363a38dc1f68f180db8a625fa1c869febff7
Gino Bezzina <bezzinag@yahoo.com>
2026-01-15 12:42:11 +0100

    2d-arrays

---
+++ 25-2d-arrays/2darrays.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int numbers[][4] ={{4,3,2,1}, 
+                       {1,2,3,4}, 
+                       {6,7,8,9}};
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            printf("%d ",numbers[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
